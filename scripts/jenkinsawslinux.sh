a5d85414fed7f8243593f1f28cbf8237265a7c48
Monzur Elahi <86269177+monzureelahi@users.noreply.github.com>
2024-07-12 18:56:36 -0400

    Create jenkinsawslinux.sh

---
+++ scripts/jenkinsawslinux.sh
@@ -0,0 +1,20 @@
+##Launch AWS Linux 2024 EC2 Instance (Source: https://www.jenkins.io/doc/book/installing/linux/)
+#Add Port 22, 8080 in inbound Port
+
+##Steps to create and run the script file
+#Create a script file (ends with .sh) by copying and pasting codes below
+#assign permission: chmod 777 scriptfilename.sh
+#run the script file: ./scriptfilename.sh
+
+##After the installation is completed, Go to browser, enter publicipaddress:8080
+
+#!/bin/bash
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-17-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
