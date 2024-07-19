48ae8c8f32d574fc4cdb7f6b19c7549dfb834d2d
Monzur Elahi <86269177+monzureelahi@users.noreply.github.com>
2024-07-19 17:57:57 -0400

    Update tomcatawslinux.sh

--- scripts/tomcatawslinux.sh
+++ scripts/tomcatawslinux.sh
@@ -9,7 +9,7 @@
 
 ##After the installation is completed, Go to browser, enter publicipaddress:8080
 
-##Install Tomcat Server
+##Install Tomcat Server (Script)
 
 #!/bin/bash
 sudo yum update -y
@@ -22,13 +22,32 @@ sudo mv tomcat /opt
 sudo chmod +x /opt/tomcat/bin/startup.sh
 sudo chmod +x /opt/tomcat/bin/shutdown.sh
 
-
 ##Start the Tomcat - Do the following manually
 $ sudo su -
 # sudo /opt/tomcat/bin
 # ./startup.sh
 
 ##Go to browser and test http://publicipaddress:8080 to check Tomcat Server
+
+##Add comment to block access for local host only starts with valve (first two line)
+# nano /opt/tomcat/webapps/host-manager/META-INF/context.xml
+# nano /opt/tomcat/webapps/manager/META-INF/context.xml
+
+Comment starts with <!--
+Comment ends with -->
+
+# nano /opt/tomcat/conf/tomcat-users.xml
+## Copy and paste the code at the end of the file before the last tag
+
+<role rolename="manager-gui"/>
+<role rolename="manager-script"/>
+<role rolename="manager-jmx"/>
+<role rolename="manager-status"/>
+<user username="admin" password="admin" roles="manager-gui, manager-script, manager-jmx, manager-status"/>
+<user username="deployer" password="deployer" roles="manager-script"/>
+<user username="tomcat" password="s3cret" roles="manager-gui"/>
+
+#Go to browser and test http://publicipaddress:8080 to check Tomcat Server, check Server Status (User ID tomcat, Password S3cret)
 
 ## Download sample .war file from https://tomcat.apache.org/tomcat-10.1-doc/appdev/sample/
 # cd /opt/tomcat/webapps
