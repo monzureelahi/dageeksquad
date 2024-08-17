2bcc43e5119a28c9e3a922746db2fe97d4dfeb8c
Monzur Elahi <86269177+monzureelahi@users.noreply.github.com>
2024-08-17 19:36:19 -0400

    Create security_group.tf

---
+++ tf/security_group.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "piit_sg_ssh" {
+  name = "piit-tf1-ec2"
+
+  #Incoming traffic
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #replace it with your ip address
+  }
+
+  #Outgoing traffic
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
