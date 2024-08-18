4ca7ad1c47cca90f273090fae0e922c9c4132b33
Monzur Elahi <86269177+monzureelahi@users.noreply.github.com>
2024-08-18 12:50:09 -0400

    Update ec2instance.tf

--- tf/ec2instance.tf
+++ tf/ec2instance.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "piit-ec2-renew" {
+resource "aws_instance" "piit-ec2" {
   ami = "ami-0e9107ed11be76fde"
   instance_type = "t2.micro"
   key_name = "piit05042024Home"
-  security_groups = ["piit-tf1-ec2-renew"]
+  security_groups = ["piit-tf1-ec2"]
 }
