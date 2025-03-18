b82450fc0b2f96580a08b7b647f4dcfaf3aee48e
Monzur Elahi <86269177+monzureelahi@users.noreply.github.com>
2025-03-18 15:09:29 -0400

    Update tomcat10awslinux.sh

--- scripts/tomcat10awslinux.sh
+++ scripts/tomcat10awslinux.sh
@@ -2,8 +2,8 @@
 
 sudo yum update –y
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.39/bin/apache-tomcat-10.1.39.tar.gz
-sudo tar -xvzf apache-tomcat-10.1.26.tar.gz
-sudo mv apache-tomcat-10.1.26 tomcat
+sudo tar -xvzf apache-tomcat-10.1.39.tar.gz
+sudo mv apache-tomcat-10.1.39.tar.gz tomcat
 sudo mv tomcat /opt
 cd /opt/tomcat
 ls -al
