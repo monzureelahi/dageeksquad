f150fa52243e509c20069937dc45e3dd252998f8
Monzur Elahi <86269177+monzureelahi@users.noreply.github.com>
2025-03-18 14:57:58 -0400

    Update tomcat10awslinux.sh

--- scripts/tomcat10awslinux.sh
+++ scripts/tomcat10awslinux.sh
@@ -1,13 +1,9 @@
 ##Installing Tomcat in AWS Linux 2
 
-#!/bin/bash
 sudo yum update –y
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.26/bin/apache-tomcat-10.1.26.tar.gz
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.39/bin/apache-tomcat-10.1.39.tar.gz
 sudo tar -xvzf apache-tomcat-10.1.26.tar.gz
 sudo mv apache-tomcat-10.1.26 tomcat
 sudo mv tomcat /opt
-
-
-
 cd /opt/tomcat
 ls -al
